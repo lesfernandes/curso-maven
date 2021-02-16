52529ce4f300a924dc33b45090eb8d2ce0960e46
lesfernandes <lemayara16@gmail.com>
2021-02-16 15:44:15 -0300

    criando um projeto com maven na linha de comando

---
+++ produtos/src/main/java/br/com/leticia/java/App.java
@@ -0,0 +1,13 @@
+package br.com.leticia.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ produtos/src/main/java/br/com/leticia/java/Produto.java
@@ -0,0 +1,6 @@
+package br.com.leticia.java;
+
+public class Produto 
+{
+    
+}
