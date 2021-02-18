66270e36061c2dcf9ddeeff810da058349c72551
lesfernandes <lemayara16@gmail.com>
2021-02-18 14:49:12 -0300

    plugin pmd

--- produtos/src/main/java/br/com/leticia/java/App.java
+++ produtos/src/main/java/br/com/leticia/java/App.java
@@ -4,10 +4,11 @@ package br.com.leticia.java;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		
+		Produto produto = new Produto("7belos", 0.15);
+		
+		System.out.println("A bala que eu gosto é: " + produto.getNome());
+	}
 }
